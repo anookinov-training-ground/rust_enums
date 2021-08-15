99d529e5adbb5159f0141aa2b76f39a78029baf4
Alexander Nookinov <anookinov@gmail.com>
2021-08-15 20:01:28 +0700

    add Option enum

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,27 @@ enum IpAddrEnum {
     V6(String),
 }
 
+// enum Message {
+//     Quit,
+//     Move { x: i32, y: i32 },
+//     Write(String),
+//     ChangeColor(i32, i32, i32),
+// }
+
+// impl Message {
+//     fn call(&self) {
+//         // method body would be defined here
+//     }
+// }
+
+// struct QuitMessage; // unit struct
+// struct MoveMessage {
+//     x: i32,
+//     y: i32
+// }
+// struct WriteMessage(String); // tuple struct
+// struct ChangeColorMessage(i32, i32, i32); // tuple struct
+
 fn main() {
     let four = IpAddrKind::V4;
     let six = IpAddrKind::V6;
@@ -41,6 +62,19 @@ fn main() {
 
     println!("{:#?}", home2);
     println!("{:#?}", loopback2);
+
+    // let m = Message::Write(String::from("hello"));
+    // m.call();
+
+    let some_number = Some(5);
+    let some_string = Some("a string");
+
+    let absent_number: Option<i32> = None;
+    println!("{:?}, {:?}, {:?}", some_number, some_string, absent_number);
+
+    let x: i8 = 5;
+    let y: Option<i8> = Some(5);
+    let sum = x + y;
 }
 
 fn route(ip_kind: IpAddrKind) {
