ae5da6ec94b9245fe3243a7d3a7aa42390b7f6ad
Alexander Nookinov <anookinov@gmail.com>
2021-08-15 20:26:49 +0700

    add matching with Option<T>

--- src/main.rs
+++ src/main.rs
@@ -110,6 +110,18 @@ fn main() {
     value_in_cents(Coin::Dime);
     value_in_cents(Coin::Quarter(UsState::Alabama));
     value_in_cents(Coin::Quarter(UsState::Alaska));
+
+    fn plus_one(x: Option<i32>) -> Option<i32> {
+        match x {
+            None => None,
+            Some(i) => Some(i + 1),
+        }
+    }
+
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+    println!("{:?}, {:?}", six, none);
 }
 
 fn route(ip_kind: IpAddrKind) {
