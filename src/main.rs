669ff40b3ef0ef2f64dcb06879fc7982097f16df
Alexander Nookinov <anookinov@gmail.com>
2021-08-15 22:17:43 +0700

    add if let

--- src/main.rs
+++ src/main.rs
@@ -131,6 +131,17 @@ fn main() {
         7 => println!("seven"),
         _ => (),
     }
+
+    // let some_u8_value = Some(0u8);
+    // match some_u8_value {
+    //     Some(3) => println!("three"),
+    //     _ => (),
+    // }
+
+    let some_u8_value = Some(0u8);
+    if let Some(3) = some_u8_value {
+        println!("three");
+    }
 }
 
 fn route(ip_kind: IpAddrKind) {
